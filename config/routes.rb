9be03b42da6d4bea7880907ff683b3a2c4753894
Alex <alex.beciana@gmail.com>
2020-12-23 18:40:49 -0500

    created migrations for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :savings
+  resources :expenses
+  resources :incomes
+  resources :goals
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
